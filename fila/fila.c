58488bf16351f8e033fc627ede4b9c7483b0bbab
Murilo Mazur <158498943+eoMazur@users.noreply.github.com>
2024-03-08 13:38:29 -0300

    Implementação de função

--- fila/fila.c
+++ fila/fila.c
@@ -29,6 +29,8 @@ void insere_elemento(struct fila *fila){
     
     printf("\nInforme o valor: ");
     scanf("%d", &novo->num);
+
+    novo->prox = NULL;
     
     if(fila->inicio == NULL){
         fila->inicio = novo;
@@ -47,37 +49,161 @@ void imprime_fila(struct fila *fila){
     
    struct elemento *aux = fila->inicio;
     int i = 0;
+    if(fila->inicio == NULL){
+
+        printf("\nFila vazia!!");
+    }
+
+    else{
+
     do{
         printf("\n%dº valor: %d", i + 1, aux->num);
+
+        if(aux->prox != NULL){
+            printf("\n    Proximo: %d", aux->prox->num);
+        }
+
+        else{
+            printf("\n  Proximo: NULL");
+        }
         aux = aux->prox;
         i++;
     }while(aux != NULL);
     
     printf("\nTodos os elementos foram impressos!!");
+
+    }
 }
 
 
+void remove_elemento(struct fila *fila){
+    if(fila->inicio == NULL){
+        printf("Fila vazia!!!");
+    }
+
+    else{
+        struct elemento *aux = fila->inicio;
+
+        fila->inicio = fila->inicio->prox;
+
+        free(aux);
+
+        printf("\nElemento removido!!");
+
+        
+    }
+}
+
+void retorna_valores_maior_media_menor(struct fila *fila){
+    if(fila->inicio == NULL){
+        printf("\nFila vazia!!");
+    }
+
+    else{
+        struct elemento *aux = fila->inicio;
+        int maior = aux->num;
+
+    
+
+        do
+        {
+            if(aux->prox != NULL){
+                if(aux->num < aux->prox->num){
+                if(maior < aux->prox->num){
+                    maior = aux->prox->num;
+                }
+            }
+            }
+
+            aux = aux->prox;
+        }while(aux != NULL);
+
+        printf("\nO maior valor da fila é: %d", maior);
+    
+        //pedaço do calculo da média;
+
+                
+        aux = fila->inicio;
+        int media = 0, count = 0;
+
+
+        do{
+           
+            media = media + aux->num;
+            count++;
+
+            aux = aux->prox;
+        }while(aux != NULL);
+
+        media = media / count;
+
+        printf("\nA média é: %d", media);
+
+        
+
+
+        //pedaço do calculo do menor valor;
+
+        
+        aux = fila->inicio;
+        int menor = aux->num;
+
+        do{
+            if(aux->prox != NULL){
+                if(aux->num > aux->prox->num){
+                    if(menor > aux->prox->num){
+                        menor = aux->prox->num;
+                    }
+                }
+
+            }
+
+            aux = aux->prox;
+        }while(aux != NULL);
+
+        printf("\nO menor valor é: %d", menor);
+        
+        
+    }
+
+    
+
+    
+}
+
 int main(void){
     struct fila fila;
+
+    cria_fila(&fila);
     while(1){
+
         
         int opcao = 0;
         
-        printf("\nEscolha uma opção: \n1 - Cria fila\n2 - Insere elemento\n3 - Imprime fila\n");
+        printf("\nEscolha uma opção:\n1 - Insere elemento\n2 - Imprime fila\n3 - Remove elemento\n4 - Retorna maior\n");
         scanf("%d", &opcao);
         
         switch(opcao){
         
-            case 1:
+            /*case 1:
                 cria_fila(&fila);
                 break;
-                
-            case 2:
+                */
+            case 1:
                 insere_elemento(&fila);
                 break;
                 
-            case 3:
+            case 2:
                 imprime_fila(&fila);
+                break;
+
+            case 3:
+                remove_elemento(&fila);
+                break;
+
+
+            case 4:
+                retorna_valores_maior_media_menor(&fila);
                 break;
                 
             default:

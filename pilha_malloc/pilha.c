17008a130c346d3475ce9b880fc45c3942adbc8d
Murilo Mazur <158498943+eoMazur@users.noreply.github.com>
2024-03-13 18:10:04 -0300

    adição da função de remoção

--- pilha_malloc/pilha.c
+++ pilha_malloc/pilha.c
@@ -44,6 +44,14 @@ void empilha(struct pilha *pilha){
 }
 
 void imprime_pilha(struct pilha *pilha){
+    if(pilha->topo == NULL){
+        printf("\nPilha vazia!!");
+    }
+
+    else if(pilha->topo != NULL){
+
+    
+
     struct elemento *aux;
     int i = 1;
 
@@ -56,7 +64,47 @@ void imprime_pilha(struct pilha *pilha){
         i++;
         aux = aux->ant;
     }while(aux != NULL);
+    }
 
+    else{
+        printf("\nErro na leitura!!");
+    }
+}
+
+void desempilha(struct pilha *pilha){
+    if(pilha->topo == NULL){
+        printf("\nPilha vazia!!");
+    }
+
+    else if(pilha->topo != NULL){
+
+        struct elemento *aux;
+
+        aux = pilha->topo;
+
+        //desempilha de for o unico elemento da pilha
+        if(aux->ant == NULL){
+            pilha->topo = NULL;
+            printf("\nValor desempilhado: %d", aux->num);
+            free(aux);
+        }
+
+        //desempilha se houver mais de um elemento
+        else if(aux->ant != NULL){
+            pilha->topo = aux->ant;
+            printf("\nValor desempilhado: %d", aux->num);
+            free(aux);
+        }
+
+        //para detecção de erros
+        else{
+            printf("\nErro de leitura dentro da verificação!!");
+        }
+    }
+
+    else{
+        printf("\nErro na leitura");
+    }
 }
 
 int main(void){
@@ -64,11 +112,35 @@ int main(void){
 
     cria_pilha(&pilha);
 
-    for(int i = 0; i < 5; i++){
-        empilha(&pilha);
+    int opcao;
+
+    while(1){
+        printf("\nEscolha a operação: ");
+        printf("\n1 - Empilhar: ");
+        printf("\n2 - Desempilhar: ");
+        printf("\n3 - Imprimir pilha: ");
+        printf("\n");
+
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            empilha(&pilha);
+            break;
+
+        case 2:
+            desempilha(&pilha);
+            break;
+
+        case 3:
+            imprime_pilha(&pilha);
+            break;
+        
+        default:
+            break;
+        }
+
+
     }
-
-    imprime_pilha(&pilha);
-
-    printf("\n\n");
 }
